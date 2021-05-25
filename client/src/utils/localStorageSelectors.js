b6ef026dff97b300070e8b3e517d7230285f1b25
Patryk Rosiak <patryyk21@gmail.com>
2021-05-25 14:38:30 +0200

    Remove unnecessary code

--- client/src/utils/localStorageSelectors.js
+++ client/src/utils/localStorageSelectors.js
@@ -1,26 +1,10 @@
 // eslint-disable-next-line import/no-cycle
-import { user } from "store/user/userSelectors";
-import { getItemFromStorage } from "utils/localStorage";
+import { user } from 'store/user/userSelectors';
+import { getItemFromStorage } from 'utils/localStorage';
 
-export const CREATED_TEMPLATES = `createdTemplates`;
-export const USER = `playwinuser`;
-export const DRAFT_TEMPLATES = `draftTemplates`;
-export const TEMPLATE_TYPE_LEAGUE = `league`;
-export const TEMPLATE_TYPE_TOURNAMENT = `tournament`;
-export const PLAYWIN_TEMPLATE_KEYS = `playwintemplatekeys`;
+export const CREATED_TEMPLATES = 'createdTemplates';
+export const USER = 'mockuser';
 
 export const getUser = getItemFromStorage(USER) || {};
 
 export const getUserId = () => user.id || null;
-
-export const getCreatedTemplates = getItemFromStorage(CREATED_TEMPLATES) || [];
-
-export const getDraftTemplates = getItemFromStorage(DRAFT_TEMPLATES) || {};
-
-export const getDraftLeagues = () => Object.values(getDraftTemplates).filter(
-  ({ type }) => type === TEMPLATE_TYPE_LEAGUE,
-);
-
-export const getDraftTournaments = () => Object.values(getDraftTemplates).filter(
-  ({ type }) => type === TEMPLATE_TYPE_TOURNAMENT,
-);
